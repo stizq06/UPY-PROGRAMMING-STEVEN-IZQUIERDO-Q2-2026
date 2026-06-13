452cc42fb226ed629f39c3fd1e1f0557ecef0b55
stizq06 <stevenizq06@gmail.com>
2026-06-12 21:57:31 -0600

    Delete Average grade.py

--- Average grade.py
+++
@@ -1,8 +0,0 @@
-#Input
-grade1=float(input("write the grade of your first parcial: "))
-grade2=float(input("write the grade of your second parcial: "))
-grade3=float(input("write the grade of your third parcial: "))
-#Process
-average = (grade1+grade2+grade3)/3 
-#Output
-print (f"This is your Grade Average {average:.2f}")
